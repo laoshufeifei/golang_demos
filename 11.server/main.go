8991ad8cec30c91de92716e28d439aa96f3c1575
liudf <liudf@mapbar.com>
2019-07-12 19:20:45 +0800

    tweak

--- 11.server/main.go
+++ 11.server/main.go
@@ -4,8 +4,9 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 // A fundamental concept in `net/http` servers is
@@ -15,37 +16,39 @@ import (
 // on functions with the appropriate signature.
 func hello(w http.ResponseWriter, req *http.Request) {
 
-    // Functions serving as handlers take a
-    // `http.ResponseWriter` and a `http.Request` as
-    // arguments. The response writer is used to fill in the
-    // HTTP response. Here out simple response is just
-    // "hello\n".
-    fmt.Fprintf(w, "hello\n")
+	// Functions serving as handlers take a
+	// `http.ResponseWriter` and a `http.Request` as
+	// arguments. The response writer is used to fill in the
+	// HTTP response. Here out simple response is just
+	// "hello\n".
+	fmt.Fprintf(w, "hello\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
-    // This handler does something a little more
-    // sophisticated by reading all the HTTP request
-    // headers and echoing them into the response body.
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+	// This handler does something a little more
+	// sophisticated by reading all the HTTP request
+	// headers and echoing them into the response body.
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+
+	fmt.Fprintf(w, "TimeStamp: %v\n", time.Now().UnixNano())
 }
 
 func main() {
 
-    // We register our handlers on server routes using the
-    // `http.HandleFunc` convenience function. It sets up
-    // the *default router* in the `net/http` package and
-    // takes a function as an argument.
-    http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
+	// We register our handlers on server routes using the
+	// `http.HandleFunc` convenience function. It sets up
+	// the *default router* in the `net/http` package and
+	// takes a function as an argument.
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 
-    // Finally, we call the `ListenAndServe` with the port
-    // and a handler. `nil` tells it to use the default
-    // router we've just set up.
-    http.ListenAndServe(":8090", nil)
+	// Finally, we call the `ListenAndServe` with the port
+	// and a handler. `nil` tells it to use the default
+	// router we've just set up.
+	http.ListenAndServe(":8090", nil)
 }
