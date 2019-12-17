71d2ce1e7b47bb096f8fe0113ad553484b440d10
liudf <liudf@mapbar.com>
2019-12-17 18:55:24 +0800

    add comment for prime filter

--- 16.prime_filter/main.go
+++ 16.prime_filter/main.go
@@ -30,8 +30,60 @@ func PrimeFilter(in <-chan int, prime int) chan int {
 }
 
 func main() {
+	// // 2, 3, 4, 5, 6, 7...
+	// originCh := make(chan int)
+	// go func() {
+	// 	for i := 2; ; i++ {
+	// 		originCh <- i
+	// 	}
+	// }()
+
+	// prime2 := <-originCh
+	// fmt.Println(prime2)
+	// filter2Ch := make(chan int)
+	// go func() {
+	// 	for {
+	// 		i := <-originCh
+	// 		if i%prime2 != 0 {
+	// 			// without 2, 4, 6, 8...
+	// 			// 3, 5, 7, 9, 11...
+	// 			filter2Ch <- i
+	// 		}
+	// 	}
+	// }()
+
+	// prime3 := <-filter2Ch
+	// fmt.Println(prime3)
+	// filter3Ch := make(chan int)
+	// go func() {
+	// 	for {
+	// 		i := <-filter2Ch
+	// 		if i%prime3 != 0 {
+	// 			// without 3, 9...
+	// 			// 5, 7
+	// 			filter3Ch <- i
+	// 		}
+	// 	}
+	// }()
+
+	// prime5 := <-filter3Ch
+	// fmt.Println(prime5)
+	// filter5Ch := make(chan int)
+	// go func() {
+	// 	for {
+	// 		i := <-filter3Ch
+	// 		if i%prime5 != 0 {
+	// 			// without 5, 15
+	// 			// 7
+	// 			filter5Ch <- i
+	// 		}
+	// 	}
+	// }()
+
+	// prime7 := <-filter5Ch
+	// fmt.Println(prime7)
+
 	ch := GenerateNatural() // 自然数序列: 2, 3, 4, ...
-	// time.Sleep(3 * time.Second)
 	for i := 0; i < 10; i++ {
 		prime := <-ch // 新出现的素数
 		fmt.Printf("%v: %v\n", i+1, prime)
